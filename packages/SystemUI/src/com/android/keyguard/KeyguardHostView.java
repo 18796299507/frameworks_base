13e925dba3d6af5fb4d0fbefa3169a79b92171d3
sunhao <932961454@qq.com>
2018-08-22 13:13:11 +0800

    KeyguardHostView: Dismiss keyguard automatically when face is recognized

--- packages/SystemUI/src/com/android/keyguard/KeyguardHostView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardHostView.java
@@ -61,7 +61,7 @@ public class KeyguardHostView extends FrameLayout implements SecurityCallback {
     protected LockPatternUtils mLockPatternUtils;
     private OnDismissAction mDismissAction;
     private Runnable mCancelAction;
-
+    private KeyguardUpdateMonitor mKeyguardUpdateMonitor;
     private final KeyguardUpdateMonitorCallback mUpdateCallback =
             new KeyguardUpdateMonitorCallback() {
 
@@ -94,6 +94,13 @@ public class KeyguardHostView extends FrameLayout implements SecurityCallback {
                 }
             }
         }
+        @Override
+        public void onTrustChanged(int userId) {
+            if (userId != KeyguardUpdateMonitor.getCurrentUser()) return;
+            if (mKeyguardUpdateMonitor.getUserCanSkipBouncer(userId) && mKeyguardUpdateMonitor.getUserHasTrust(userId)){
+                dismiss(false, userId);
+            }
+        }
     };
 
     // Whether the volume keys should be handled by keyguard. If true, then
@@ -111,6 +118,7 @@ public class KeyguardHostView extends FrameLayout implements SecurityCallback {
 
     public KeyguardHostView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mKeyguardUpdateMonitor = KeyguardUpdateMonitor.getInstance(context);
         KeyguardUpdateMonitor.getInstance(context).registerCallback(mUpdateCallback);
     }
 
